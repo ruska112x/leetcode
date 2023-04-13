3d5a34eae9e78a96d0a43668cf762ea16ef10706
Ruslan Karabalin <karabalin112@gmail.com>
2023-04-13 10:40:30 +0600

    [*] 9

---
+++ 9-PalindromeNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Solution {
+ public:
+  bool isPalindrome(int x) {
+    bool result = false;
+    if (x >= 0) {}
+    return result;
+  }
+};
+
+int main() {
+  Solution solution;
+  return 0;
+}
