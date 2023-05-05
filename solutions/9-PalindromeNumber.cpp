15c95d360f719f5fece33f7bd5292e357c82cd70
Ruslan Karabalin <karabalin112@gmail.com>
2023-05-05 09:14:51 +0600

    [+] 9

--- solutions/9-PalindromeNumber.cpp
+++ solutions/9-PalindromeNumber.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 
 class Solution {
- public:
-  bool isPalindrome(int x) {
-    bool result = false;
-    if (x >= 0) {}
-    return result;
-  }
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        auto str = std::to_string(x);
+        for (int i = 0; i < str.length(); ++i) {
+            if(str[i] != str[str.length() - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
 
 int main() {
   Solution solution;
+  std::cout << solution.isPalindrome(123321) << "\n";
   return 0;
 }
