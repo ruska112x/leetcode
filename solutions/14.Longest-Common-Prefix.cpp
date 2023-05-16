271a747e893f089d7649f9dd85d37088bb357f9c
Ruslan Karabalin <karabalin112@gmail.com>
2023-05-16 09:28:08 +0600

    [+] 14

--- solutions/14.Longest-Common-Prefix.cpp
+++ solutions/14.Longest-Common-Prefix.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 #include <vector>
 #include <string>
@@ -5,28 +6,33 @@
 using namespace std;
 
 class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        size_t len = strs.at(0).length();
-        for (size_t i = 1; i < strs.size(); i++)
-        {
-            if (len > strs.at(i).length()) {
-                len = strs.at(i).length();
+    public:
+        string longestCommonPrefix(vector<string>& strs) {
+            size_t len = strs.at(0).length();
+            for (size_t i = 1; i < strs.size(); i++) {
+                if (len > strs.at(i).length()) {
+                    len = strs.at(i).length();
+                }
             }
+            string result;
+            result.reserve(len);
+            for (size_t i = 0; i < len; i++) {
+                for (size_t j = 1; j < strs.size(); j++) {
+                    if (strs.at(0).at(i) != strs.at(j).at(i)) {
+                        return result;
+                    }
+                }            
+                result.push_back(strs.at(0).at(i));
+            }
+            return result;
         }
-        for (string str : strs)
-        {
-            
-        }
-        
-    }
 };
 
 int main() {
     Solution solution;
     vector<string> strs = {"flower","flow","flight"};
-  cout << solution.longestCommonPrefix(strs)
-       << "\n";
-  return 0;
+    cout << solution.longestCommonPrefix(strs)
+        << "\n";
+    return 0;
 }
 
