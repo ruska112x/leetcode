95116b90740fccbcf8617fa2a7fa351e324b4fa4
Ruslan Karabalin <karabalin112@gmail.com>
2023-05-17 20:03:50 +0600

    [+] solve 50

--- cpp/50.Pow(x,n).cpp
+++ cpp/50.Pow(x,n).cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,18 +8,21 @@ using namespace std;
 class Solution {
  public:
   double myPow(double x, int n) {
-    if (n == 0 || abs(x - 1) <= 0.000000001) {
+    if (n == 0) {
       return 1;
     }
+    if (n == INT_MAX) {
+      return x == 1 ? 1 : x == -1 ? -1 : 0;
+    } else if (n == INT_MIN) {
+      return x == 1 ? 1 : x == -1 ? 1 : 0;
+    }
     double result = 1;
-    if (n > 0) {
-      for (size_t i = 0; i < n; i++) {
-        result *= x;
-      }
-    } else {
-      for (size_t i = 0; i < abs(n); i++) {
-        result *= 1 / x;
-      }
+    if (n < 0) {
+      x = 1 / x;
+      n = -n;
+    }
+    for (size_t i = 0; i < n; i++) {
+      result *= x;
     }
     return result;
   }
@@ -26,6 +30,6 @@ class Solution {
 
 int main() {
   Solution solution;
-  cout << solution.myPow(2, -2) << "\n";
+  cout << solution.myPow(0.00001, 2147483647) << "\n";
   return 0;
 }
