99816e081205934a28e5955f2633ed39b618e002
Ruslan Karabalin <karabalin112@gmail.com>
2024-03-16 11:39:07 +0600

    feat: 525

--- cpp/2485.Find-the-Pivot-Integer.cpp
+++ cpp/2485.Find-the-Pivot-Integer.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 class Solution {
-public:
-    int pivotInteger(int n) {
-        for(int i = 1; i <= n; ++i) {
-            int p = (1+i)*i/2;
-            int s = (i+n)*(n-i+1)/2;
-            if(s==p) {
-                return i;
-                }
-            }
-        return -1;
+ public:
+  int pivotInteger(int n) {
+    for (int i = 1; i <= n; ++i) {
+      int p = (1 + i) * i / 2;
+      int s = (i + n) * (n - i + 1) / 2;
+      if (s == p) {
+        return i;
+      }
     }
+    return -1;
+  }
 };
 
 int main() {
