d5f8c1a3d7d73256d2464bd0762405f71b025c35
Ruslan Karabalin <karabalin112@gmail.com>
2024-08-11 19:23:44 +0600

    feat: add 0169

--- rust/s0169_majority_element/src/lib.rs
+++ rust/s0169_majority_element/src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashMap;
+
+pub fn majority_element(nums: Vec<i32>) -> i32 {
+    let n = (nums.len() as f32 / 2.0).floor() as i32;
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    let mut result = -1;
+    for e in nums {
+        let e_count = map.get(&e);
+        if e_count.is_none() {
+            map.insert(e, 1);
+        } else {
+            let count = *e_count.unwrap();
+            if count > n {
+                result = e;
+                break;
+            }
+            map.insert(e, count + 1);
+        }
+    }
+    if result == -1 {
+        for k in map.keys() {
+            let c = *map.get(&k).unwrap();
+            if c > n {
+                result = *k;
+            }
+        }
+    }
+    return result;
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::majority_element;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_majority_element_00() {
+        assert_eq!(majority_element(vec![3, 2, 3]), 3);
+    }
+
+    #[test]
+    fn test_majority_element_01() {
+        assert_eq!(majority_element(vec![2, 2, 1, 1, 1, 2, 2]), 2);
     }
 }
