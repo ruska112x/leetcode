0abcc1483ff9c7ad4c0a6cf2115026cbf8cddc39
Ruslan Karabalin <karabalin112@gmail.com>
2024-08-14 23:18:25 +0600

    feat: solve 1929

---
+++ java-solutions/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "org.karabalin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.junit:junit-bom:5.10.0"))
+    implementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
