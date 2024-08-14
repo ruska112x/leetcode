9dcf8b6a6fddfaf8d2a7aff3bc70638dd11a1d4b
Ruslan Karabalin <karabalin112@gmail.com>
2024-08-14 22:40:23 +0600

    fix: 0058

--- solutions/src/main/kotlin/s0058/Solution0058.kt
+++ solutions/src/main/kotlin/s0058/Solution0058.kt
@@ -2,7 +2,7 @@ package s0058
 
 class Solution {
     fun lengthOfLastWord(s: String): Int {
-        val strings = s.split("\\s+".toRegex())
+        val strings = s.trim().split("\\s+".toRegex())
         return strings.last().length
     }
 }
@@ -10,4 +10,5 @@ class Solution {
 fun main() {
     val s = Solution()
     println(s.lengthOfLastWord("Hello World") == 5)
+    println(s.lengthOfLastWord("   fly me   to   the moon  ") == 4)
 }
