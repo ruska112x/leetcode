e56e2d5f3a55078d490936ddc039e86d7284a18a
Karabalin Ruslan <karabalin112@gmail.com>
2025-04-16 21:51:32 +0600

    feat: add more solutions

---
+++ kotlin/src/main/kotlin/org/karabalin/f0001t1000/f0401t0500/s0404/Solution0404.kt
@@ -0,0 +1,44 @@
+package org.karabalin.f0001t1000.f0401t0500.s0404
+
+import java.util.*
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+class Solution {
+    fun sumOfLeftLeaves(root: TreeNode?): Int {
+        if (root == null) return 0
+
+        val queue: Queue<Pair<TreeNode, Boolean>> = LinkedList()
+        queue.offer(Pair(root, false))
+        var totalSum = 0
+
+        while (queue.isNotEmpty()) {
+            val (node, isLeft) = queue.poll()
+
+            if (isLeft && node.left == null && node.right == null) {
+                totalSum += node.`val`
+            }
+
+            node.left?.let { queue.offer(Pair(it, true)) }
+            node.right?.let { queue.offer(Pair(it, false)) }
+        }
+
+        return totalSum
+    }
+}
+
+fun main() {
+    val root = TreeNode(3).apply {
+        left = TreeNode(9)
+        right = TreeNode(20).apply {
+            left = TreeNode(15)
+            right = TreeNode(7)
+        }
+    }
+
+    val solution = Solution()
+    println(solution.sumOfLeftLeaves(root))
+}
