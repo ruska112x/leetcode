94a1758ca50a57d6542d6ae31274b6c18a26dec8
Karabalin Ruslan <karabalin112@gmail.com>
2025-04-16 18:34:24 +0600

    feat: remove cpp, rust and java

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "org.karabalin"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
