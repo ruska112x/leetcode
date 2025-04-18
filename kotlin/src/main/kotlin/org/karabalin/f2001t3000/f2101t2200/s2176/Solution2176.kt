4c9ce1c8b54f6274fc8ea3823b7e7fcff0fd45f2
Karabalin Ruslan <karabalin112@gmail.com>
2025-04-18 16:53:20 +0600

    feat: add s0038, s2176, s2807

---
+++ kotlin/src/main/kotlin/org/karabalin/f2001t3000/f2101t2200/s2176/Solution2176.kt
@@ -0,0 +1,20 @@
+package org.karabalin.f2001t3000.f2101t2200.s2176
+
+class Solution {
+    fun countPairs(nums: IntArray, k: Int): Int {
+        var count = 0
+        for (i in 0 until nums.size - 1) {
+            for (j in (i + 1) until nums.size) {
+                if (nums[i] == nums[j] && ((i * j) % k == 0)) {
+                    ++count
+                }
+            }
+        }
+        return count
+    }
+}
+
+fun main() {
+    val solution = Solution()
+    println(solution.countPairs(intArrayOf(3, 1, 2, 2, 2, 1, 3), 2))
+}
