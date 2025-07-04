32ee022b3304b6e82c65710757f5dd2ebae9be0e
Karabalin Ruslan <karabalin112@gmail.com>
2025-07-04 10:13:25 +0600

    feat: add s0021

---
+++ golang/s0021/s0021.go
@@ -0,0 +1,37 @@
+package s0021
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+
+	var prevHead = &ListNode{}
+
+	current := prevHead
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			current.Next = list1
+			list1 = list1.Next
+		} else {
+			current.Next = list2
+			list2 = list2.Next
+		}
+		current = current.Next
+	}
+
+	if list1 != nil {
+		current.Next = list1
+	} else {
+		current.Next = list2
+	}
+
+	return prevHead.Next
+}
